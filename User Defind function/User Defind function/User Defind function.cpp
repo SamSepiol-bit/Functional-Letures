541fd502835b111e5b12b69840f04ec866a5010a
SamSepiol-bit <icbtworkfor@gmail.com>
2024-07-09 12:03:43 +0530

    Add files via upload

---
+++ User Defind function/User Defind function/User Defind function.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+using namespace std;
+
+//Function prototype
+void greet();
+
+int main() {
+
+	//Function call
+	greet();
+
+	return 0;
+}
+
+//Function Definition
+void greet() {
+	cout << "Hello friend!" << endl;
+}
